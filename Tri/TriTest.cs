fe352aa1481abf1180492791f84bb921b8338798
sherlock.chen53 <sherlock.chen53@TI03NB136>
2019-05-13 00:12:43 +0800

    fix typo

--- Tri/TriTest.cs
+++ Tri/TriTest.cs
@@ -6,48 +6,48 @@ namespace Tri
     [TestFixture]
     public class TriTest
     {
-        public Tri Tringle;
+        public Tri Triangle;
         [SetUp]
         public void SetUp()
         {
-            Tringle = new Tri();
+            Triangle = new Tri();
         }
 
         [Test]
         public void NotTriangle()
         {
-            Assert.AreEqual("not triangle", Tringle.GetType(1, 2, 3));
+            Assert.AreEqual("not triangle", Triangle.GetType(1, 2, 3));
         }
 
         [Test]
         public void RegularTriangle()
         {
-            Assert.AreEqual("regular triangle", Tringle.GetType(3, 3, 3));
+            Assert.AreEqual("regular triangle", Triangle.GetType(3, 3, 3));
         }
 
         [Test]
         public void IsoscelesTriangle()
         {
-            Assert.AreEqual("acute isosceles triangle", Tringle.GetType(3, 3, 2));
-            Assert.AreEqual("obtuse isosceles triangle", Tringle.GetType(3, 3, 5));
+            Assert.AreEqual("acute isosceles triangle", Triangle.GetType(3, 3, 2));
+            Assert.AreEqual("obtuse isosceles triangle", Triangle.GetType(3, 3, 5));
         }
 
         [Test]
         public void ObtuseTriangle()
         {
-            Assert.AreEqual("obtuse triangle", Tringle.GetType(3, 4, 6));
+            Assert.AreEqual("obtuse triangle", Triangle.GetType(3, 4, 6));
         }
 
         [Test]
         public void AcuteTriangle()
         {
-            Assert.AreEqual("acute triangle", Tringle.GetType(10, 19, 21));
+            Assert.AreEqual("acute triangle", Triangle.GetType(10, 19, 21));
         }
 
         [Test]
         public void RightTriangle()
         {
-            Assert.AreEqual("right triangle", Tringle.GetType(3, 4, 5));
+            Assert.AreEqual("right triangle", Triangle.GetType(3, 4, 5));
         }
 
         [Test]
@@ -59,16 +59,16 @@ namespace Tri
         [TestCase(5, 3, 4)]
         public void SwapInputOrder(int edge1, int edge2, int edge3)
         {
-            Assert.AreEqual("right triangle", Tringle.GetType(edge1, edge2, edge3));
+            Assert.AreEqual("right triangle", Triangle.GetType(edge1, edge2, edge3));
         }
 
         [Test]
         public void InputNearIntegerMax()
         {
-            Assert.AreEqual("regular triangle", Tringle.GetType(int.MaxValue, int.MaxValue, int.MaxValue));
-            Assert.AreEqual("acute isosceles triangle", Tringle.GetType(int.MaxValue-1, int.MaxValue-1, int.MaxValue));
-            Assert.AreEqual("not triangle", Tringle.GetType(unchecked(int.MaxValue + 1), int.MaxValue - 1, int.MaxValue));
-            Assert.AreEqual("acute triangle", Tringle.GetType(int.MaxValue -2, int.MaxValue - 1, int.MaxValue));
+            Assert.AreEqual("regular triangle", Triangle.GetType(int.MaxValue, int.MaxValue, int.MaxValue));
+            Assert.AreEqual("acute isosceles triangle", Triangle.GetType(int.MaxValue-1, int.MaxValue-1, int.MaxValue));
+            Assert.AreEqual("not triangle", Triangle.GetType(unchecked(int.MaxValue + 1), int.MaxValue - 1, int.MaxValue));
+            Assert.AreEqual("acute triangle", Triangle.GetType(int.MaxValue -2, int.MaxValue - 1, int.MaxValue));
         }
     }
 }
